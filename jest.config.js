a3680f649e1770f008adaca0b5b76c6c5a0403d1
Joon-ha <iinow@naver.com>
2021-01-04 20:32:33 +0900

    gitflow-feature-stash: codecov (#4)
    * gitflow-feature-stash: codecov
    
    * gitflow-feature-stash: codecov md 수정

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(ts|js)?$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.{ts,ts,js,js}', '!src/**/*.d.ts'],
+  // roots: ["src/**/*"],
+  // transform: {
+  //   '^.+\\.ts?$': 'ts-jest',
+  // },
+  // collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.ts'],
+  // moduleFileExtensions: ["ts", "js", "json"],
+  testRegex: '(test/.*|(\\.|/)(test|spec))\\.(ts)?$',
+  // coverageDirectory: 'coverage',
 }
